d9663b1583826fedf4af7a277df3d0481e9b1310
chetverikkk <71694921+chetverikkk@users.noreply.github.com>
2020-09-22 12:38:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import sys
 from selenium import webdriver
 import time
-from bs4 import BeautifulSoup
 link = ""
 browser = webdriver.Chrome()
 browser.get(link)
-time.sleep(10)
+time.sleep(5)
 while True:
-    for i in range:
-        coef = browser.find_elements_by_css_selector('a.graph-label > span')
-        time.sleep(10)
-        coef2 = browser.find_elements_by_css_selector('a.graph-label > span')
-        if coef[i] != coef2[i]:
-            print(coef[i].text)
+    coef = browser.find_elements_by_css_selector('a.graph-label:nth-child(1) > span:nth-child(1)')
+    time.sleep(15)
+    coef2 = browser.find_elements_by_css_selector('a.graph-label:nth-child(2) > span:nth-child(1)')
+    if coef != coef2:
+        print(coef[0].text)
